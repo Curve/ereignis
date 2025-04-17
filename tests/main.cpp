26e45b6cffe767cc1734d6551f01e915a254ea53
Curve <fynnbwdt@gmail.com>
2025-04-17 12:41:11 +0200

    docs(logo): fix light / dark

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,10 @@
 #include <boost/ut.hpp>
 
+#include <ereignis/manager/manager.hpp>
+
 int main()
 {
+    auto event = ereignis::event<0, void(int)>{};
+
     return boost::ut::cfg<boost::ut::override>.run();
 }
