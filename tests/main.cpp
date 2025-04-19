4b7bc88d39597ac09f6c5b0973c179ab9ccdde19
Curve <fynnbwdt@gmail.com>
2025-04-19 21:51:44 +0200

    refactor(tests): clean up

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,6 @@
 #include <boost/ut.hpp>
 
-#include <ereignis/manager/manager.hpp>
-
 int main()
 {
-    auto event = ereignis::event<0, void(int)>{};
-
     return boost::ut::cfg<boost::ut::override>.run();
 }
